e27b14f78802b373a841a145678faca6051f6e96
Franco-Pedrazzi <yuro2105@gmail.com>
2026-04-30 11:35:35 -0300

    agacharse

--- Objets/Characters/States/crouched.gd
+++ Objets/Characters/States/crouched.gd
@@ -1,1 +1,19 @@
 extends State_base
+
+func on_physics_process(delta: float) -> void:
+	controlled_node.velocity.x=0
+	controlled_node.animated_sprite_2d.play("Crouched")
+
+	
+func on_input(event: InputEvent) -> void:
+	if Input.is_action_pressed("Crouch"):
+		if not (Input.is_action_pressed("Left") or Input.is_action_pressed("Right")) and Input.is_action_pressed("Crouch"):
+			state_machine.change_to("Crouched")
+		elif (Input.is_action_pressed("Left") or Input.is_action_pressed("Right")) and Input.is_action_pressed("Crouch"):
+			state_machine.change_to("Roll")
+			
+	if not Input.is_action_pressed("Crouch"):
+		state_machine.change_to("Idle")
+	
+	if Input.is_action_pressed("Jump"):
+		state_machine.change_to("Jump")
